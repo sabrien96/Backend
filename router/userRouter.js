1c61ab0fa6fc039f420e26b021710294a7d0b176
mohamed ali <alinemrmohamed@gmail.com>
2021-04-26 18:22:33 +0200

    back register-checkout routes and controller check

--- router/userRouter.js
+++ router/userRouter.js
@@ -1,12 +1,14 @@
 
 const express = require('express');
-const { authenticate,register,getAll,getCurrent,getById,update,remove} = require('../controllers/UserController');
+const { authenticate,register,registerCheckOut,getAll,getCurrent,getById,update,remove} = require('../controllers/UserController');
 const { auth } = require('../middleware/authMiddleware');
 const router = express.Router();
 
 // routes
 router.post('/authenticate', authenticate);
 router.post('/register', register);
+//registerCheckOut
+router.post('/registerCheckOut', registerCheckOut);
 router.get('/', auth,getAll);
 router.get('/current', getCurrent);
 router.get('/:id', getById);
